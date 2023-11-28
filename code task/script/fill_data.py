6467e630317752c57c0cac278f7bb1202eca2bed
Oidaho <76991612+Oidaho@users.noreply.github.com>
2023-11-28 14:32:17 +1000

    ADD: Добавлен макет генератора данных

--- code task/script/fill_data.py
+++ code task/script/fill_data.py
@@ -4,10 +4,44 @@ Main code file
 from data import DataBase
 
 db = DataBase()
-files = []
+
+
+class Generator:
+    """
+    Generator class for custom data.
+    """
+
+    def __init__(self):
+        pass
+
+    def generate_cars(self):
+        """_summary_
+        """
+
+    def generate_drivers(self):
+        """_summary_
+        """
+
+    def generate_violations(self):
+        """_summary_
+        """
+
+    def generate_fine(self):
+        """_summary_
+        """
+
 
 if __name__ == "main":
-    print(db)
-    for file in files:
-        pass
-    
+    gen = Generator()
+
+    for car in gen.generate_cars():
+        db.cars.insert()
+
+    for driver in gen.generate_drivers():
+        db.drivers.insert()
+
+    for fine in gen.generate_fine():
+        db.fine.insert()
+
+    for violation in gen.generate_violations():
+        db.violations.insert()
