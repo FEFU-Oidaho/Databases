f7ba2571df10c747a69f8db1820039e2b1b605ec
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-01-08 01:21:39 +1000

    UPDATE: Добавлен макет веб приложения

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 """
 Main code file
 """
-from data import DataBase
+from flask import (
+    Flask,
+    render_template
+)
 
 
-db = DataBase()
+app = Flask(__name__)
 
-if __name__ == "__mani__":
-    print("Hello Wordl!")
+
+@app.route("/")
+def show_mainpage():
+    main_path = "main.html"
+    return render_template(main_path)
+
+
+@app.route("/<selected_role>")
+def role_seelctor(selected_role):
+
+    pages = [
+        ("driver", "1"),
+        ("inspector", "2"),
+        ("administrator", "3")
+    ]
+
+    for role, page in pages:
+        if selected_role == role:
+            return page
+
+    return "Not found"
+
+
+app.run(
+    host="localhost",
+    port=8000,
+    debug=True
+)
