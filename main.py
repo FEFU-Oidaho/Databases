d61b12637500f2c487dda3c365e335000090ee78
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-01-08 02:48:24 +1000

    UPPDATE: Макет сайта со стилями

--- main.py
+++ main.py
@@ -3,33 +3,54 @@ Main code file
 """
 from flask import (
     Flask,
-    render_template
+    render_template,
+    abort,
+    redirect,
+    url_for
 )
 
 
 app = Flask(__name__)
 
+\
+menu = [
+    {"name": "Главная", "url": "home"},
+    {"name": "Приложение водителя", "url": "driver"},
+    {"name": "Приложение инспектора", "url": "inspector"},
+    {"name": "Приложение администратора", "url": "administrator"},
+]
 
 @app.route("/")
-def show_mainpage():
+def redirect_to_main():
+    return redirect(url_for("show_homepage"))
+
+
+@app.route("/home")
+def show_homepage():
     main_path = "main.html"
-    return render_template(main_path)
+    return render_template(
+        main_path,
+        menu=menu
+    )
 
 
 @app.route("/<selected_role>")
 def role_seelctor(selected_role):
 
     pages = [
-        ("driver", "1"),
-        ("inspector", "2"),
-        ("administrator", "3")
+        ("driver", "driver.html"),
+        ("inspector", "inspector.html"),
+        ("administrator", "administrator.html")
     ]
 
-    for role, page in pages:
+    for role, page_path in pages:
         if selected_role == role:
-            return page
+            return render_template(
+                page_path,
+                menu=menu
+            )
 
-    return "Not found"
+    abort(404)
 
 
 app.run(
